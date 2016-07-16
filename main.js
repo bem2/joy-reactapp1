da67d3fbbed5a6afebf4530d64dea40d7a07817c
bem2 <abemgi2@gmail.com>
2016-07-17 02:33:18 +0530

    updates
    simply changed

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import App from './App.jsx';
 
 ReactDOM.render(
     <div>
+    <App user="Joy">How are you?.</App>
+    <App user="Bem">Where do you live?</App>
     </div>, 
     document.getElementById('app')
 );
