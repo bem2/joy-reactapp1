19c378fed2051fa6909644b7dd96c3ea788eec49
bem2 <abemgi2@gmail.com>
2016-07-17 01:09:41 +0530

    Adding styles in React
    Adding styles in React

--- App.jsx
+++ App.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
+    var bemStyle = {
+         border: '1px solid red',
+         padding: '30px',
+         color: '#666'
+      }
+       
       return (
-         <div>
+         <div style={bemStyle}>
             <h1>Hello {this.props.user}</h1> 
             <p>{this.props.children}</p>
          </div>
